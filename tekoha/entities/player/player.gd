72c7ccef6f2932d5e179fdac486c81d399fa228c
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-11-11 14:36:11 -0300

    feat: script player

---
+++ tekoha/entities/player/player.gd
@@ -0,0 +1,77 @@
+class_name Player
+extends CharacterBody2D
+
+signal player_dead()
+
+const SPEED : float = 100.0
+const ROLL_SPEED : float = 120.0
+
+var move_direction : Vector2
+var mouse_direction : Vector2
+var facing_direction : Vector2 = Vector2.RIGHT
+var attack_direction : Vector2
+var roll_direction : Vector2
+
+var can_roll : bool = true
+
+var roll_cooldown : float = 2.0
+
+var anim_transition : int = 0
+
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var state_machine: StateMachine = $StateMachine
+
+func _ready() -> void:
+	# Conectar sinais
+	health_component.died.connect(_on_player_died)
+	hitbox_component.attack_received.connect(_on_player_attack_received)
+	# Iniciar state_machine
+	state_machine.init(self)
+	# Adicionar ao grupo "player"
+	self.add_to_group("player")
+
+func _process(_delta: float) -> void:
+	
+	pass
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+
+func get_direction() -> Vector2:
+	return Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+
+func get_mouse_direction() -> Vector2:
+	return (get_global_mouse_position() - global_position).normalized()
+
+func check_attack_input():
+	if Input.is_action_just_pressed("attack"):
+		if state_machine.current_state.name != "AttackEnd":
+			state_machine.current_state.transition_to("attack1")
+		else:
+			state_machine.current_state.transition_to("attack2")
+
+func check_roll_input():
+	if Input.is_action_just_pressed("roll") and can_roll:
+		state_machine.current_state.transition_to("roll")
+
+func die():
+	# TODO: ver possivel dependencia com health_component
+	await get_tree().create_timer(.3).timeout
+	queue_free()
+	player_dead.emit()
+
+func _on_player_died():
+	# Transicionar para DEATH
+	state_machine.current_state.transition_to("death")
+
+func _on_player_attack_received(attack_data: AttackData):
+	# Sofrer o dano 
+	health_component.take_damage(attack_data)
+	# Passar os dados do ataque para o stun_state e transicionar para STUN
+	var stun_state : StunState = state_machine.states.get("stun")
+	if state_machine.current_state.name not in ["stun", "death"]:
+		stun_state.receive_attack_data(attack_data)
+		state_machine.current_state.transition_to("stun")
