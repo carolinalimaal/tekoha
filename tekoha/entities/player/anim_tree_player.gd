b832affee4b04c1867ec879115dd6d7cf2977164
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-11-11 14:36:34 -0300

    feat: script animation tree player

---
+++ tekoha/entities/player/anim_tree_player.gd
@@ -0,0 +1,22 @@
+extends AnimationTree
+
+func _ready() -> void:
+	self.active = true
+
+func _process(_delta: float) -> void:
+	match owner.state_machine.current_state.name:
+		"Idle":
+			set("parameters/Idle/blend_position", owner.facing_direction)
+		"Walk":
+			set("parameters/Walk/blend_position", owner.move_direction)
+		"Roll":
+			set("parameters/Roll/blend_position", owner.move_direction)
+		"Attack1":
+			set("parameters/Attack_1/blend_position", owner.move_direction)
+		"AttackEnd":
+			set("parameters/Attack_End/blend_position", owner.move_direction)
+		"Attack2":
+			set("parameters/Attack_2/blend_position", owner.move_direction)
+		"Stun":
+			set("parameters/Stun_Start/blend_position", -owner.velocity)
+			set("parameters/Stun_Loop/blend_position", -owner.velocity)
