4a3c55de89246f045f2278255f666491b012d0c5
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-11-11 14:37:52 -0300

    feat: attack_end state player

---
+++ tekoha/entities/player/states/state_attack_end_player.gd
@@ -0,0 +1,25 @@
+class_name AttackEnd
+extends State
+
+func _enter() -> void:
+	# Conectar o sinal animation_finished
+	owner_node.animation_tree.animation_finished.connect(_on_animation_finished)
+	# Atualizar variavel que controla animacoes
+	owner_node.anim_transition = 4
+
+func _exit() -> void:
+	# Desconectar o sinal de animation_finished (evitar conflito com outros estados)
+	owner_node.animation_tree.animation_finished.disconnect(_on_animation_finished)
+
+func _update(_delta: float) -> void:
+	# TODO: isso sera arrumado depois 
+	owner_node.check_roll_input()
+	owner_node.check_attack_input()
+
+func _physics_update(_delta: float) -> void:
+	pass
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	if anim_name in ["attack_end_down", "attack_end_left", "attack_end_right", "attack_end_up"]:
+		transition_to("idle")
+		return
