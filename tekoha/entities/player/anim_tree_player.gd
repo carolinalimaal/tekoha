1e36b3fd35be876e8fc7b43ae6cdcbb388040784
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-11-11 17:14:23 -0300

    fix: direção das animações de ataque
    Correção da variável que indica a direção para as animações:
    - Attack_1
    - AttackEnd
    - Attack_2

--- tekoha/entities/player/anim_tree_player.gd
+++ tekoha/entities/player/anim_tree_player.gd
@@ -12,11 +12,11 @@ func _process(_delta: float) -> void:
 		"Roll":
 			set("parameters/Roll/blend_position", owner.move_direction)
 		"Attack1":
-			set("parameters/Attack_1/blend_position", owner.move_direction)
+			set("parameters/Attack_1/blend_position", owner.attack_direction)
 		"AttackEnd":
-			set("parameters/Attack_End/blend_position", owner.move_direction)
+			set("parameters/Attack_End/blend_position", owner.attack_direction)
 		"Attack2":
-			set("parameters/Attack_2/blend_position", owner.move_direction)
+			set("parameters/Attack_2/blend_position", owner.attack_direction)
 		"Stun":
 			set("parameters/Stun_Start/blend_position", -owner.velocity)
 			set("parameters/Stun_Loop/blend_position", -owner.velocity)
