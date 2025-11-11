3b37975a52dcec7382152bf9e9096a2c3e9f1b84
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-11-11 14:29:47 -0300

    feat: state base

---
+++ tekoha/utils/state_machine/state.gd
@@ -0,0 +1,22 @@
+class_name State 
+extends Node
+
+signal Transitioned(state_name: String)
+
+@onready var owner_node: CharacterBody2D
+@onready var state_machine: StateMachine
+
+func _enter() -> void:
+	pass
+
+func _exit() -> void:
+	pass
+
+func _update(_delta: float) -> void:
+	pass
+
+func _physics_update(_delta: float) -> void:
+	pass
+
+func transition_to(state_name: String):
+	Transitioned.emit(state_name)
