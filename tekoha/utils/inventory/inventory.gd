9d5789ac4125136e36bd0218c906d87e7aa95c63
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-11-14 17:03:41 -0300

    feat: criação inventory

---
+++ tekoha/utils/inventory/inventory.gd
@@ -0,0 +1,50 @@
+class_name Inventory
+extends Node
+# TODO: Instanciar o inventario em algum lugar, provavelmente em um script global
+var inventory_slots: Array[ItemSlot]
+
+var _size: int = 6
+
+func _init() -> void:
+	# Criar inventario com 6 slots
+	for i in range(_size):
+		inventory_slots.append(ItemSlot.new(i))
+
+# Adiciona um item ao inventario
+func add_item(item: ConsumableItemData) -> bool:
+	# Pegar um slot vazio e, caso encontre um, adiciona o item a ele
+	var slot: ItemSlot = _get_empty_item_slot()
+	if !slot: 
+		return false
+	slot.item = item
+	# Emite sinais para atualizar a UI
+	GlobalSignals.updated_inventory.emit()
+	return true
+
+# Remove um item do inventario
+func remove_item_from_slot(index: int) -> void:
+	# Verifica se o index é válido
+	if index < 0 or index >= inventory_slots.size():
+		return
+	# Pegar um slot pelo index e, caso contenha um item, limpa-lo
+	var slot: ItemSlot = _get_slot_by_index(index)
+	if slot.is_empty():
+		return
+	slot.clear()
+	# Emite sinais para atualizar a UI
+	GlobalSignals.updated_inventory.emit()
+
+# Retorna um slot vazio
+func _get_empty_item_slot() -> ItemSlot:
+	for slot in inventory_slots:
+		if slot.is_empty():
+			return slot
+	return null
+
+# Retorna um slot pelo index
+func _get_slot_by_index(index: int) -> ItemSlot:
+	# Verifica se o index é válido
+	if index < 0 or index >= inventory_slots.size():
+		return null
+	# Retorna o slot
+	return inventory_slots[index]
