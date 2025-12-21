1840c7a951a5bb59c323251066cf99afced8a0f7
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-12-21 17:49:44 -0300

    refactor: attack_state inimigo sacola

--- tekoha/entities/enemies/sacola/states/attack_state.gd
+++ tekoha/entities/enemies/sacola/states/attack_state.gd
@@ -1,7 +1,20 @@
-class_name MeleeAttackEnemyState 
 extends State
 
+var _attack_cooldown_timer: Timer
+
+func _ready() -> void:
+	# Criar o timer, conectar sinal de timeout e adicionar o timer na arvore
+	_attack_cooldown_timer = Timer.new()
+	_attack_cooldown_timer.one_shot = true
+	_attack_cooldown_timer.autostart = false
+	_attack_cooldown_timer.timeout.connect(_on_attack_cooldown_timer_timeout)
+	add_child(_attack_cooldown_timer)
+
 func _enter():
+	# Atualizar a flag
+	owner_node.can_attack = false
+	# Gerar um cooldown aleatorio
+	_attack_cooldown_timer.wait_time = randf_range(0.8, 2.0)
 	# Registra que há um inimigo atacando
 	owner_node.register_attacker()
 	owner_node.velocity = Vector2.ZERO
@@ -12,6 +25,8 @@ func _enter():
 	owner_node.animation_tree.animation_finished.connect(_on_animation_tree_animation_finished)
 
 func _exit():
+	# Iniciar timer do cooldown
+	_attack_cooldown_timer.start()
 	# Remove o registro do inimigo atacando
 	owner_node.deregister_attacker()
 	owner_node.velocity = Vector2.ZERO
@@ -29,9 +44,12 @@ func _physics_update(_delta: float):
 func _attack():
 	# Habilitar a hurtbox_collision
 	owner_node.hurtbox_component.hurtbox_collision.set_deferred("disabled", false)
-	owner_node.velocity = owner_node.move_direction * owner_node.attack_speed
+	#owner_node.velocity = owner_node.move_direction * owner_node.attack_speed
 
 func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if anim_name in ["attack_down", "attack_up", "attack_left", "attack_right"] and GlobalRefs.player:
-		transition_to("circling")
+		transition_to("Aiming")
 		return
+
+func _on_attack_cooldown_timer_timeout() -> void:
+	owner_node.can_attack = true
