6a6329197bb244cc6f5c2aaded3d7d96c524ce1a
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-12-31 20:06:19 -0300

    feat: sinal para encontrar novos muiraquitãs

--- tekoha/globals/global_signals.gd
+++ tekoha/globals/global_signals.gd
@@ -7,3 +7,5 @@ signal wallet_updated(new_amout: int)
 
 # Sinal para avisar que uma moeda foi coletada
 signal coin_collected(value: int)
+# Sinal para avisar que um muiraquita foi encontrado
+signal new_muiraquita_found(id: int)
