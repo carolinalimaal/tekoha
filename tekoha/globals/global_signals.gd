b4f15d105b7894ce569550475c7d004f6c6932b2
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-12-31 18:09:36 -0300

    feat: sinal para atualizar a HUD

--- tekoha/globals/global_signals.gd
+++ tekoha/globals/global_signals.gd
@@ -2,5 +2,8 @@ extends Node
 
 # Este script armazena sinais globais
 
+# Sinais para a UI ser atualizada
+signal wallet_updated(new_amout: int)
+
 # Sinal para avisar que uma moeda foi coletada
 signal coin_collected(value: int)
