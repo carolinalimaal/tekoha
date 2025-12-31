280ee0e625f29001bae86ba97c4cfe16103e5dfc
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-12-31 18:06:14 -0300

    feat: sistema de coleta de moedas

--- tekoha/globals/game_manager.gd
+++ tekoha/globals/game_manager.gd
@@ -1,3 +1,14 @@
 extends Node
 
 
+var wallet: int = 0
+
+func _ready() -> void:
+	GlobalSignals.coin_collected.connect(_on_coin_collected)
+
+func _on_coin_collected(value: int) -> void:
+	add_coin(value)
+
+func add_coin(value: int) -> void:
+	wallet += value
+	GlobalSignals.wallet_updated.emit(wallet)
