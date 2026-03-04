7d1bc47c3542935bab5b8cbc28bb9f7ba7a5e830
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-03-03 22:07:30 -0300

    fix: baú UI e codigo

--- tekoha/utils/bau/bau.gd
+++ tekoha/utils/bau/bau.gd
@@ -1,32 +1,37 @@
 class_name Bau extends StaticBody2D
 
 var openned: bool = false
-@onready var item: ConsumableItemData = load("res://data/items/tambaqui_assado.tres")
+var can_interact: bool = false
+@export var item: ConsumableItemData
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var interaction_container: MarginContainer = $InteractionUI/InteractionContainer
 @onready var show_item_panel: Panel = $InteractionUI/ShowItemPanel
+@onready var show_item_panel_hbox_cont: HBoxContainer = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer
 @onready var show_item_panel_label: Label = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/ShowItemLabel
 @onready var item_hbox_cont: HBoxContainer = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer
 @onready var item_image: TextureRect = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/TextureRect
+
+func _ready() -> void:
+	show_item_panel.visible = false
+	interaction_container.visible = false
 
 # Reações do baú com a proximidade do player
 func _on_player_interact_area_body_entered(body: Node2D) -> void:
 	if body is Player and !openned:
 		interaction_container.visible = true
+		can_interact = true
 
 func _on_player_interact_area_body_exited(body: Node2D) -> void:
 	if body is Player:
-		if openned:
-			close_chest()
-		else:
-			interaction_container.visible = false
+		interaction_container.visible = false
+		can_interact = false
 
 # Lógica de apertar o interact input dentro da área do baú
 func _unhandled_input(_event: InputEvent) -> void:
 	if GlobalRefs.input_manager.get_action_pressed("interact"):
 		if openned and get_tree().paused:
 			close_chest()
-		elif !openned and !get_tree().paused:
+		elif !openned and !get_tree().paused and can_interact:
 			update_item_panel()
 			open_chest()
 
@@ -46,15 +51,16 @@ func update_item_panel():
 		item_image.texture = null
 		show_item_panel_label.text = "O BAÚ ESTÁ VAZIO."
 		item_hbox_cont.visible = false
-		
+		show_item_panel_hbox_cont.alignment = BoxContainer.ALIGNMENT_BEGIN
 	elif verify_full_inventory():
 		item_image.texture = null
 		show_item_panel_label.text = "SEU INVENTÁRIO ESTÁ CHEIO."
 		item_hbox_cont.visible = false
-		
+		show_item_panel_hbox_cont.alignment = BoxContainer.ALIGNMENT_BEGIN
 	elif item:
 		item_image.texture = item.icon
 		show_item_panel_label.text = "VOCÊ DESEJA PEGAR O ITEM\n" + item.name.to_upper() + "?"
+		show_item_panel_hbox_cont.alignment = BoxContainer.ALIGNMENT_CENTER
 		if !item_hbox_cont.visible:
 			item_hbox_cont.visible = true
 
@@ -69,6 +75,7 @@ func open_chest():
 
 func close_chest():
 	show_item_panel.visible = false
+	interaction_container.visible = true
 	get_tree().paused = false
 	sprite.play_backwards("bau_animation")
 	openned = false
