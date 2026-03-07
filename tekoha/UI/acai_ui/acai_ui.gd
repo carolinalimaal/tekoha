890553a9c566e9d538bb1ee3cdaa97cecc3f4feb
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-03-07 17:53:13 -0300

    feat: carregar wallet do save na UI

--- tekoha/UI/acai_ui/acai_ui.gd
+++ tekoha/UI/acai_ui/acai_ui.gd
@@ -5,5 +5,9 @@ extends CanvasLayer
 func _ready() -> void:
 	GlobalSignals.wallet_updated.connect(_on_wallet_updated)
 	
+	# Carregar dados do save
+	if GameManager.current_save:
+		_on_wallet_updated(GameManager.current_save.wallet)
+	
 func _on_wallet_updated(wallet: int):
 	moedas.text = str(wallet)
