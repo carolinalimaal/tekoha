8a5d7f9f637276db659682c92fae441ed299d0ec
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-03-07 22:25:31 -0300

    refactor: baú com registro no GameManager

--- tekoha/utils/bau/bau.gd
+++ tekoha/utils/bau/bau.gd
@@ -1,23 +1,48 @@
 class_name Bau extends StaticBody2D
 
-var openned: bool = false
-var can_interact: bool = false
+@export var id: int
 @export var item: ConsumableItemData
+
+var is_open: bool = false
+var can_interact: bool = false
+
+@onready var player_interact_area: Area2D = $PlayerInteractArea
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var interaction_container: MarginContainer = $InteractionUI/InteractionContainer
 @onready var show_item_panel: Panel = $InteractionUI/ShowItemPanel
-@onready var show_item_panel_hbox_cont: HBoxContainer = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer
-@onready var show_item_panel_label: Label = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/ShowItemLabel
-@onready var item_hbox_cont: HBoxContainer = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer
-@onready var item_image: TextureRect = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/TextureRect
+@onready var show_item_label: Label = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/ShowItemLabel
+@onready var item_image: TextureRect = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/ItemImage
+
+@onready var buttons_container: HBoxContainer = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/ButtonsContainer
+@onready var accept_button: Button = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/ButtonsContainer/AcceptButton
+@onready var decline_button: Button = $InteractionUI/ShowItemPanel/Bg/MarginContainer/HBoxContainer/VBoxContainer/ButtonsContainer/DeclineButton
+
 
 func _ready() -> void:
+	player_interact_area.body_entered.connect(_on_player_interact_area_body_entered)
+	player_interact_area.body_exited.connect(_on_player_interact_area_body_exited)
+	
+	accept_button.pressed.connect(_on_accept_button_pressed)
+	decline_button.pressed.connect(_on_decline_button_pressed)
+	
 	show_item_panel.visible = false
 	interaction_container.visible = false
+	
+	if GameManager.current_save.opened_chests.has(id):
+		item = null
+
+# Lógica de apertar o interact input dentro da área do baú
+func _unhandled_input(_event: InputEvent) -> void:
+	if InputManager.get_action_pressed("interact"):
+		if is_open and get_tree().paused:
+			close_chest()
+		elif !is_open and !get_tree().paused and can_interact:
+			update_item_panel()
+			open_chest()
 
 # Reações do baú com a proximidade do player
 func _on_player_interact_area_body_entered(body: Node2D) -> void:
-	if body is Player and !openned:
+	if body is Player and !is_open:
 		interaction_container.visible = true
 		can_interact = true
 
@@ -26,19 +51,12 @@ func _on_player_interact_area_body_exited(body: Node2D) -> void:
 		interaction_container.visible = false
 		can_interact = false
 
-# Lógica de apertar o interact input dentro da área do baú
-func _unhandled_input(_event: InputEvent) -> void:
-	if InputManager.get_action_pressed("interact"):
-		if openned and get_tree().paused:
-			close_chest()
-		elif !openned and !get_tree().paused and can_interact:
-			update_item_panel()
-			open_chest()
-
 # Verifica o aperto dos botões e as ações
 func _on_accept_button_pressed() -> void:
 	GlobalRefs.inventory.add_item(item)
-	print("Item adicionado ao invenário!")
+	# Registra o bau aberto no current_save
+	GameManager.current_save.opened_chests[id] = true
+	print("Item adicionado ao inventário!")
 	item = null
 	close_chest()
 
@@ -49,36 +67,35 @@ func _on_decline_button_pressed() -> void:
 func update_item_panel():
 	if item == null:
 		item_image.texture = null
-		show_item_panel_label.text = "O BAÚ ESTÁ VAZIO."
-		item_hbox_cont.visible = false
-		show_item_panel_hbox_cont.alignment = BoxContainer.ALIGNMENT_BEGIN
+		item_image.visible = false
+		show_item_label.text = "O BAÚ ESTÁ VAZIO."
+		buttons_container.visible = false
 	elif verify_full_inventory():
 		item_image.texture = null
-		show_item_panel_label.text = "SEU INVENTÁRIO ESTÁ CHEIO."
-		item_hbox_cont.visible = false
-		show_item_panel_hbox_cont.alignment = BoxContainer.ALIGNMENT_BEGIN
+		item_image.visible = false
+		show_item_label.text = "SEU INVENTÁRIO ESTÁ CHEIO."
+		buttons_container.visible = false
 	elif item:
 		item_image.texture = item.icon
-		show_item_panel_label.text = "VOCÊ DESEJA PEGAR O ITEM\n" + item.name.to_upper() + "?"
-		show_item_panel_hbox_cont.alignment = BoxContainer.ALIGNMENT_CENTER
-		if !item_hbox_cont.visible:
-			item_hbox_cont.visible = true
+		show_item_label.text = item.name.to_upper() + " ENCONTRADO!"
+		if !buttons_container.visible:
+			buttons_container.visible = true
 
 # Funções para abrir e fechar o baú e suas respectivas animações
 func open_chest():
 	get_tree().paused = true
 	sprite.play("bau_animation")
 	await sprite.animation_finished
-	openned = true
+	is_open = true
 	interaction_container.visible = false
 	show_item_panel.visible = true
 
 func close_chest():
-	show_item_panel.visible = false
-	interaction_container.visible = true
 	get_tree().paused = false
 	sprite.play_backwards("bau_animation")
-	openned = false
+	is_open = false
+	interaction_container.visible = true
+	show_item_panel.visible = false
 
 # Função que verifica se o inventário está cheio
 func verify_full_inventory() -> bool:
