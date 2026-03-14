c2f9ebc116ae5e05d1c2027dc056fb39516e063a
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-03-14 13:29:29 -0300

    feat: testes com camera

--- tekoha/levels/casa.gd
+++ tekoha/levels/casa.gd
@@ -15,5 +15,27 @@ func _ready() -> void:
 	#camera.limit_top = 500
 	#camera.limit_left = 0
 	#camera.limit_right = 0
+
+	# script pra pegar o ponto central do tilemap
+	#var tilemap: TileMapLayer = $Tiles/HouseFloor
+	#
+	#var rect = tilemap.get_used_rect()
+	#var tile_size = tilemap.tile_set.tile_size
+	#
+	#var size_pixels = rect.size * tile_size
+	#var pos_pixels = rect.position * tile_size
+	#
+	#var center_local = pos_pixels + size_pixels / 2
+	#var center_global = tilemap.to_global(center_local)
+#
+	#print(center_global)
 	
-	pass
+	var camera: PlayerCamera = $Player/PlayerCamera
+	
+	set_limits_camera(camera)
+
+func set_limits_camera(camera: PlayerCamera) -> void:
+	camera.limit_bottom = 363
+	camera.limit_top = -75
+	camera.limit_right = 539
+	camera.limit_left = -140
