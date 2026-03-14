a50edf6970253dd1e2caa3d4c7a36897497867a2
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-03-13 22:00:14 -0300

    feat: limites de camera + iluminação

---
+++ tekoha/levels/casa.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#var level = get_parent()
+	#var level_parent = level.get_parent()
+	#
+	#var player_node = level_parent.get_child(1)
+	#var camera: Camera2D = player_node.get_child(1)
+	
+	#var camera: Camrea2D = GlobalRefs.camera (ideia)
+	
+	#camera.limit_bottom = -500
+	#camera.limit_top = 500
+	#camera.limit_left = 0
+	#camera.limit_right = 0
+	
+	pass
