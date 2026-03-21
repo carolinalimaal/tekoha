fdc66f8004b5f51afffbd460459392587f350e79
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-03-21 20:42:53 -0300

    fix: organizando repo

--- tekoha/globals/main_scene/ui.gd
+++ tekoha/globals/main_scene/ui.gd
@@ -3,8 +3,7 @@ extends CanvasLayer
 @onready var pause_menu: Control = $PauseMenu
 @onready var game_over_menu: Control = $GameOverMenu
 @onready var inventory_ui: InventoryUI = $InventoryUi
-@onready var heart_ui: CanvasLayer = $HeartUI
-@onready var acai_ui: CanvasLayer = $AcaiUI
+@onready var hud: Control = $HUD
 @onready var new_item_found_panel: NewItemFoundPanel = $NewItemFoundPanel
 
 
