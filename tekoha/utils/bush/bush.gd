2a235fb880d6b48a0cae060cbdb75563f6b92b75
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-03-25 20:59:31 -0300

    feat: adicionado terrain do tileset

--- tekoha/utils/bush/bush.gd
+++ tekoha/utils/bush/bush.gd
@@ -3,11 +3,12 @@ extends StaticBody2D
 var player_inside: Player
 
 func _process(_delta) -> void:
+	
 	#expulsa o player caso ele vá para dentro do arbusto
 	if player_inside and player_inside.state_machine.current_state.name != "Roll":
 		var direction = (player_inside.global_position - global_position).normalized()
 		player_inside.global_position += direction * 10
-
+		
 
 func _on_player_roll_area_body_entered(body: Node2D) -> void:
 	if body is Player and body.state_machine.current_state.name == "Roll":
