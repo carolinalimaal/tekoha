67b46585fa2e168ce5ab7a5c5b3ceb26c9268378
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-03-27 00:29:30 -0300

    feat: novo sistema para diálogos e cutscenes

---
+++ tekoha/UI/dialogue/dialogue_control.gd
@@ -0,0 +1,129 @@
+extends CanvasLayer
+
+signal dialogue_started
+signal dialogue_ended
+
+enum Idiom {
+	PT, 
+	EN
+}
+
+@export var language: Idiom = Idiom.PT
+@export var typing_speed: float = 0.05
+
+var is_showing: bool
+var current_dialogue: Array[DialogueLine]
+var index: int
+var type_tween: Tween
+var active_text_label: RichTextLabel
+var player: Player
+
+@onready var solid_background: ColorRect = $SolidBackground
+@onready var cutscene_background: TextureRect = $CutsceneBackground
+@onready var dialogue_box: PanelContainer = $DialogueBox
+@onready var speech_text_dialogue: RichTextLabel = $DialogueBox/MarginContainer/SpeechTextDialogue
+@onready var cutscene_box: PanelContainer = $CutsceneBox
+@onready var speech_text_cutscene: RichTextLabel = $CutsceneBox/MarginContainer/SpeechTextCutscene
+
+func _ready() -> void:
+	solid_background.hide()
+	cutscene_background.hide()
+	dialogue_box.hide()
+	cutscene_box.hide()
+	is_showing = false
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if InputManager.get_action_pressed("ui_accept") and is_showing:
+		_next_sentence()
+
+func start_speech(dialogue_data: DialogueSettings) -> void:
+	if !is_showing:
+		dialogue_box.show()
+		is_showing = true
+		current_dialogue = dialogue_data.dialogues
+		index = 0
+		
+		#if player:
+			#player.is_paused = true
+		
+		_show_sentence()
+		dialogue_started.emit()
+
+func _show_sentence() -> void:
+	var current = current_dialogue[index]
+	var is_cutscene = current.background_image != null
+	
+	var text_to_show = _set_text_to_show(current)
+	match language:
+		Idiom.PT:
+			text_to_show += current.text_pt
+		Idiom.EN:
+			text_to_show += current.text_en
+	
+	_set_background_image(current, is_cutscene)
+	
+	active_text_label.text = text_to_show
+	active_text_label.visible_characters = 0
+	
+	if type_tween and type_tween.is_running():
+		type_tween.kill()
+	
+	type_tween = create_tween()
+	var duration = text_to_show.length() * typing_speed
+	type_tween.tween_property(active_text_label, "visible_characters", text_to_show.length(), duration)
+
+func _next_sentence() -> void:
+	if !is_showing:
+		return
+	
+	if type_tween and type_tween.is_running():
+		type_tween.kill()
+		active_text_label.visible_characters = -1
+		return
+	
+	if index < current_dialogue.size() - 1:
+		index += 1
+		_show_sentence()
+	else:
+		_end_speech()
+
+func _end_speech() -> void:
+	solid_background.hide()
+	cutscene_background.hide()
+	dialogue_box.hide()
+	cutscene_box.hide()
+	is_showing = false
+	current_dialogue = []
+	#if player:
+		#player.is_paused = false
+	dialogue_ended.emit()
+
+func _set_background_image(current: DialogueLine, is_cutscene: bool) -> void:
+	if is_cutscene:
+		dialogue_box.hide()
+		cutscene_box.show()
+		solid_background.show()
+		
+		if cutscene_background.texture != current.background_image:
+			cutscene_background.texture = current.background_image
+			cutscene_background.show()
+			
+			# Animacao do background
+			cutscene_background.modulate.a = 0.0
+			var bg_tween = create_tween()
+			bg_tween.set_trans(Tween.TRANS_SINE)
+			bg_tween.tween_property(cutscene_background, "modulate:a", 1.0, 1.0)
+			
+			active_text_label = speech_text_cutscene
+	else:
+		dialogue_box.show()
+		cutscene_box.hide()
+		solid_background.hide()
+		
+		active_text_label = speech_text_dialogue
+
+func _set_text_to_show(current: DialogueLine) -> String:
+	if current.actor_name:
+		return current.actor_name + ": "
+	else:
+		return ""
