6a626da95f6a642e826e36b5d961fadce8202093
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-03-31 10:16:50 -0300

    refactor: atualização sprite potes

--- tekoha/utils/pots/pot.gd
+++ tekoha/utils/pots/pot.gd
@@ -1,9 +1,8 @@
 class_name Pote extends StaticBody2D
 
-@export var sprite: CompressedTexture2D
+@export var sprite_frames: SpriteFrames
 
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
 @onready var hitbox_collision: CollisionShape2D = $HitboxComponent/HitboxCollision
@@ -11,14 +10,14 @@ class_name Pote extends StaticBody2D
 @onready var collision: CollisionShape2D = $Collision
 
 func _ready() -> void:
-	sprite_2d.texture = sprite
+	animated_sprite_2d.sprite_frames = sprite_frames
 	
 	hitbox_component.attack_received.connect(_on_pot_attack_received)
 	health_component.died.connect(_on_pot_destroyed)
 	
 func _on_pot_attack_received(attack_data: AttackData):
 	health_component.take_damage(attack_data)
-	sprite_2d.frame = min(floor(abs(health_component.max_health - health_component.current_health) / 4), 4)
+	animated_sprite_2d.frame = min(floor(abs(health_component.max_health - health_component.current_health) / 4), 4)
 	
 func _on_pot_destroyed():
 	hitbox_component.set_deferred("monitoring", false)
