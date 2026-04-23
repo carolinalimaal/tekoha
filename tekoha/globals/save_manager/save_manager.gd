becdc12f18d0ffc08a92a9bcb7e7ab1ce1d62e01
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-04-23 14:44:45 -0300

    feat: atualização dos valores de posição e level_path

--- tekoha/globals/save_manager/save_manager.gd
+++ tekoha/globals/save_manager/save_manager.gd
@@ -12,5 +12,14 @@ func load_game() -> SaveData:
 	return null
 
 func _update_data() -> void:
-	if GlobalRefs.player and GlobalRefs.player.health_component:
-		GameManager.current_save.player_health = GlobalRefs.player.health_component.current_health
+	if GlobalRefs.player:
+		if GlobalRefs.player.health_component:
+			GameManager.current_save.player_health = GlobalRefs.player.health_component.current_health
+		GameManager.current_save.player_position = GlobalRefs.player.global_position
+	
+	var main_node = get_tree().root.find_child("Main", true, false)
+	if main_node:
+		var level_container = main_node.get_node("Level")
+		if level_container.get_child_count() > 0: 
+			var current_level = level_container.get_child(0)
+			GameManager.current_save.current_level_path = current_level.scene_file_path
