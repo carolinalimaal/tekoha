15913e4ddb8273646b20f723d01135f7912aa626
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-05-08 10:29:49 -0300

    feat: ajustes para reuniao

--- tekoha/levels/room_10/root_mecanic.gd
+++ tekoha/levels/room_10/root_mecanic.gd
@@ -1,30 +1,31 @@
-extends Node2D
+class_name RootMecanic extends Node2D
 
-@onready var torche_one: Torch = $Torches/Torche
-@onready var torche_two: Torch = $Torches/Torche2
-@onready var torche_three: Torch = $Torches/Torche3
-@onready var root: Root = $Root
-@onready var root_two: Root = $Root2
-@onready var root_three: Root = $Root3
-@onready var root_four: Root = $Root4
-@onready var root_five: Root = $Root5
-@onready var root_six: Root = $Root6
+#@onready var torche_one: Torch = $Torches/Torche
+#@onready var torche_two: Torch = $Torches/Torche2
+#@onready var torche_three: Torch = $Torches/Torche3
+#@onready var root: Root = $Root
+#@onready var root_two: Root = $Root2
+#@onready var root_three: Root = $Root3
+#@onready var root_four: Root = $Root4
+#@onready var root_five: Root = $Root5
+#@onready var root_six: Root = $Root6
+@onready var mecanic_activators: Node2D = $MecanicActivators
+@onready var roots: Node2D = $Roots
 
-var torches_off: int = 0
+@export var meta_activations: int
+var current_activations: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	torche_one.torch_turnned_off.connect(_on_torch_turned_off)
-	torche_two.torch_turnned_off.connect(_on_torch_turned_off)
-	torche_three.torch_turnned_off.connect(_on_torch_turned_off)
+	#torche_one.torch_turnned_off.connect(_on_torch_turned_off)
+	#torche_two.torch_turnned_off.connect(_on_torch_turned_off)
+	#torche_three.torch_turnned_off.connect(_on_torch_turned_off)
+	for activator in mecanic_activators.get_children():
+		activator.torch_turnned_off.connect(_on_torch_turned_off)
 
 func _on_torch_turned_off(_torch_id: int):
-	torches_off+=1
-	print(torches_off)
-	if torches_off == 3:
-		root.root_remove()
-		root_two.root_remove()
-		root_three.root_remove()
-		root_four.root_remove()
-		root_five.root_remove()
-		root_six.root_remove()
+	current_activations+=1
+	print(current_activations)
+	if current_activations == meta_activations:
+		for root in roots.get_children():
+			root.root_remove()
