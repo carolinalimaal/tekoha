40e6bfbfbeb0c096384f77be265e3fe08e9589c9
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-06-18 11:07:08 -0300

    fix: tentativa de melhora

--- tekoha/entities/enemies/sacola/sacola.gd
+++ tekoha/entities/enemies/sacola/sacola.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 	attack_range_sqr = attack_range * attack_range
 	
 	nav_agent.path_desired_distance = 5.0
-	nav_agent.target_desired_distance = attack_range
+	nav_agent.target_desired_distance = 5.0
 	nav_agent.target_position = GlobalRefs.player.position
 	
 	state_machine.init(self)
