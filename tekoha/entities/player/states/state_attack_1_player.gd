e56e615ef7fa2e4f4472757bacf5d51c535e8ff1
Pedro Castro <pedro7mirand@gmail.com>
2026-06-18 22:31:10 -0300

    refact: ataque reformulado e mais rápido

--- tekoha/entities/player/states/state_attack_1_player.gd
+++ tekoha/entities/player/states/state_attack_1_player.gd
@@ -37,5 +37,5 @@ func attack():
 
 func _on_animation_finished(anim_name: StringName) -> void:
 	if anim_name in ["attack_1_down", "attack_1_left", "attack_1_right", "attack_1_up"]:
-		transition_to("AttackEnd")
+		transition_to("Idle")
 		return
