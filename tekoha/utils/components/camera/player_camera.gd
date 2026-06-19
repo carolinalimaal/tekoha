8c42f77519df8c6f61cb078e285666a9591c37f0
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-06-19 02:19:54 -0300

    refactor: camera pos ao realizar salvamento

--- tekoha/utils/components/camera/player_camera.gd
+++ tekoha/utils/components/camera/player_camera.gd
@@ -44,7 +44,7 @@ func _on_camera_shake():
 	apply_shake()
 	
 func update_camera_stats(level: Level):
-	print(level)
+	print(level.name)
 	if level is Level:
 		limit_left = level.limit_left
 		limit_right = level.limit_right
