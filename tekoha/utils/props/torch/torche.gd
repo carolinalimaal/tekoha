0d6e49010a833fd6ec9d0bb34e1c4f2ffddee501
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-06-20 18:26:34 -0300

    feat: particulas e animação

--- tekoha/utils/props/torch/torche.gd
+++ tekoha/utils/props/torch/torche.gd
@@ -8,6 +8,8 @@ signal puzzle_activator()
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
 @onready var hitbox_collision: CollisionShape2D = $HitboxComponent/HitBoxCollision
 @onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var hit_particles: GPUParticles2D = $HitParticles
+@onready var torche_particles: GPUParticles2D = $TorcheParticles
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
 
@@ -15,8 +17,10 @@ func _ready() -> void:
 	hitbox_component.attack_received.connect(_on_torch_attack_received)
 	if GameManager.current_save.game_state >= GameManager.GameState.AFTER_PUZZLE_1:
 		animated_sprite_2d.play("third_hit")
+		torch_off_particles()
 	else:
 		animated_sprite_2d.play("default")
+		torche_particles.emitting = true
 
 func _on_torch_attack_received(_attack_data: AttackData):
 	stronger_light()
@@ -33,6 +37,7 @@ func _on_torch_attack_received(_attack_data: AttackData):
 func torch_off():
 	var tween: Tween = create_tween()
 	tween.tween_property(point_light_2d, "energy", 0, 0.2)
+	torch_off_particles()
 	hitbox_component.set_deferred("monitoring", false)
 	hitbox_collision.set_deferred("disabled", true)
 	point_light_2d.set_deferred("disabled", true)
@@ -42,8 +47,12 @@ func stronger_light():
 	var tween: Tween = create_tween()
 	var gradient_text: GradientTexture2D = point_light_2d.texture
 	
+	hit_particles.emitting = true
 	tween.tween_property(point_light_2d, "energy", 0.8, 0.1)
 	tween.tween_property(gradient_text, "fill_to", Vector2(1.15, 0), 0.1)
-	tween.tween_property(gradient_text, "fill_to", Vector2(1.0, 0), 0.1)
-	tween.tween_property(point_light_2d, "energy", 0.5, 0.1)
+	tween.tween_property(gradient_text, "fill_to", Vector2(1.0, 0), 0.3)
+	tween.tween_property(point_light_2d, "energy", 0.5, 0.3)
 	
+func torch_off_particles():
+	torche_particles.emitting = false
+	hit_particles.emitting = false
