27515bb3a6d96ddb1d834a92837f732ed35817b7
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-06-23 15:07:10 -0300

    fix: detecção de colisão da bola de papel

--- tekoha/utils/components/projectile/projectile.gd
+++ tekoha/utils/components/projectile/projectile.gd
@@ -21,6 +21,6 @@ func _physics_process(delta: float) -> void:
 func _on_visible_on_screen_notifier_screen_exited() -> void:
 	queue_free()
 
-func _on_body_entered(_body: Node2D) -> void:
-	
-	queue_free()
+func _on_body_entered(body: Node2D) -> void:
+	if body is TileMapLayer or body is Player:
+		queue_free()
