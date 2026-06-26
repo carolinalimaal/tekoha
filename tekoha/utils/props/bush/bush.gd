5fdc0bf6eec8ba0f3be2c36133974516279b2a23
deLoretoMelo <meloloretomarcelo@gmail.com>
2026-06-26 01:50:33 -0300

    feat: partículas animadas do arbusto

--- tekoha/utils/props/bush/bush.gd
+++ tekoha/utils/props/bush/bush.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 var player_inside: Player
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var gpu_particles_2d: GPUParticles2D = $Sprite2D/GPUParticles2D
 
 var bend_speed: float = 0.2
 var back_speed: float = 2
@@ -21,6 +22,6 @@ func _on_player_roll_area_body_entered(body: Node2D) -> void:
 
 func bush_anim():
 	var tween: Tween = create_tween()
-	
+	gpu_particles_2d.emitting = true
 	tween.tween_property(sprite_2d, "skew", 0.20, bend_speed).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(sprite_2d, "skew", 0.0, back_speed).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
