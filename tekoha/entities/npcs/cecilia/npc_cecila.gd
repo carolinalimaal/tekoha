8dbf7202df66cda4f6a8160b92c9551e73a8613a
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-07-15 23:40:12 -0300

    refactor: cutscene cozinha

--- tekoha/entities/npcs/cecilia/npc_cecila.gd
+++ tekoha/entities/npcs/cecilia/npc_cecila.gd
@@ -3,15 +3,18 @@ extends NPC
 @export var dialogues_list: Array[DialogueSettings]
 
 var current_dialogue_index: int = -1
+var _is_own_dialogue: bool = false
 
 func interact() -> void:
 	current_dialogue_index = dialogues_list.find(npc_dialogue)
+	_is_own_dialogue = true
 	super()
 
 func _on_timeline_ended() -> void:
 	super()
-	if current_dialogue_index == -1:
+	if !_is_own_dialogue:
 		return
+	_is_own_dialogue = false
 
 	match GameManager.current_save.game_state:
 		GameManager.GameState.TRAINING_COMPLETE:
@@ -20,5 +23,3 @@ func _on_timeline_ended() -> void:
 			GameManager.set_game_state(GameManager.GameState.SECOND_MEETING_CECILIA)
 		GameManager.GameState.IN_KITCHEN:
 			GameManager.set_game_state(GameManager.GameState.AFTER_CECILIA_GIFT)
-
-	current_dialogue_index = -1
