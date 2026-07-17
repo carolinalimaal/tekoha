22202e28af6fd49f2d6996ecbe0e11a1e7214b83
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-07-17 01:09:58 -0300

    fix: remover todo

--- tekoha/utils/props/muiraquita/muiraquita.gd
+++ tekoha/utils/props/muiraquita/muiraquita.gd
@@ -15,7 +15,6 @@ func _ready() -> void:
 func collect() -> void:
 	# Bloquear registrar duas vezes o mesmo muiraquita
 	if !GameManager.current_save.collected_muiraquitas.has(id):
-		# TODO: substituir por sfx específico do muiraquitã quando disponível
 		if GameManager.current_save.known_items.has("muiraquita"):
 			AudioManager.create_2d_audio_at_location(position, SoundEffect.SOUND_EFFECT_TYPE.MUIRAQUITA)
 		GlobalSignals.new_muiraquita_found.emit(id)
