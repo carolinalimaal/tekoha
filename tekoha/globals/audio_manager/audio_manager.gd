7898925c1de64d6b140b071a427e0ae0747b42ec
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2026-07-28 18:27:00 -0300

    fix: removendo áudio não utilizado mais

--- tekoha/globals/audio_manager/audio_manager.gd
+++ tekoha/globals/audio_manager/audio_manager.gd
@@ -117,12 +117,6 @@ func get_sound_effect_duration(type: SoundEffect.SOUND_EFFECT_TYPE) -> float:
 		return sound_effect_dict[type].sound_effect.get_length()
 	return 0.0
 
-# Inicia um som global em loop e o interrompe automaticamente apos 'duration' segundos.
-func start_looping_audio_for_duration(type: SoundEffect.SOUND_EFFECT_TYPE, duration: float) -> void:
-	start_looping_audio(type)
-	var stop_timer := get_tree().create_timer(duration, true)
-	stop_timer.timeout.connect(func(): stop_looping_audio(type))
-
 # Inicia um som global em loop
 # Nao faz nada se ja estiver tocando.
 func start_looping_audio(type: SoundEffect.SOUND_EFFECT_TYPE) -> void:
--- tekoha/levels/room_4/room_4.gd
+++ tekoha/levels/room_4/room_4.gd
@@ -1,7 +1,6 @@
 extends Level
 
 @export var bg_music: MusicTrack
-#@export var motor_loop_duration: float = 2.0
 
 @onready var path_block: PathBlock = $PathBlock
 @onready var room_5_door: Door = $Room5Door
@@ -16,12 +15,6 @@ func _ready() -> void:
 		GlobalRefs.game_nightfall_filter.show()
 		GlobalRefs.player.turn_light_on_or_off(true)
 		GlobalRefs.player.point_light_2d.energy = 0.4
-		
-		#_play_boat_going_away()
 	else:
 		path_block.set_deferred("monitoring", false)
 		room_5_door.set_deferred("monitoring", true)
-		
-
-#func _play_boat_going_away() -> void:
-	#AudioManager.start_looping_audio_for_duration(SoundEffect.SOUND_EFFECT_TYPE.BOAT_MOTOR_LOOP, motor_loop_duration)
